49f16418cb4de2ebe18f80e21422593ada5ee8ad
Amirreza Kadkhodazadeh <amirreza.dev@protonmail.com>
2025-06-10 22:27:21 +0330

    Zodiac letter modal added to the game

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,16 @@
 'use client';
 
 import { useState } from 'react';
+import {
+ Dialog,
+ DialogBackdrop,
+ DialogPanel,
+ DialogTitle,
+} from '@headlessui/react';
 
 export default function Home() {
  const [count, setCount] = useState<number | null>(null);
+ const [open, setOpen] = useState(true);
  const [shuffledRoles, setShuffledRoles] = useState<string[]>([]);
  const [currentIndex, setCurrentIndex] = useState(0);
  const [currentRole, setCurrentRole] = useState<string | null>(null);
@@ -139,6 +146,64 @@ export default function Home() {
        </li>
       ))}
      </ul>
+
+     {/* Zodiac Letter */}
+     <div className='mt-12 text-center'>
+      <button
+       onClick={() => setOpen(true)}
+       className='rounded-md bg-transparent px-2.5 py-1.5 text-sm ring-1 ring-amber-500 text-amber-500'
+      >
+       نامه زودیاک
+      </button>
+     </div>
+     <Dialog open={open} onClose={setOpen} className='relative z-10'>
+      <DialogBackdrop
+       transition
+       className='fixed inset-0 bg-gray-900 transition-opacity data-closed:opacity-0 data-enter:duration-300 data-enter:ease-out data-leave:duration-200 data-leave:ease-in'
+      />
+
+      <div className='fixed inset-0 z-10 w-screen overflow-y-auto'>
+       <div className='flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0'>
+        <DialogPanel
+         transition
+         className='relative transform overflow-hidden rounded-lg bg-gray-800 text-center shadow-xl transition-all data-closed:translate-y-4 data-closed:opacity-0 data-enter:duration-300 data-enter:ease-out data-leave:duration-200 data-leave:ease-in sm:my-8 sm:w-full sm:max-w-lg data-closed:sm:translate-y-0 data-closed:sm:scale-95'
+        >
+         <div className='bg-gray-800 px-4 pt-5 pb-4 sm:p-6 sm:pb-4'>
+          <div className='mt-3 text-center sm:mt-0 sm:mr-4'>
+           <DialogTitle as='h2' className='text-base font-semibold text-white'>
+            نامه زودیاک
+           </DialogTitle>
+           <div className='mt-2'>
+            <p className='text-sm text-white'>
+             به هیچ وجه نامه رو نبند وگرنه درجا از بازی کیک میشی! فقط نامه رو
+             بنویس و گوشی رو تحویل گرداننده بده
+            </p>
+           </div>
+          </div>
+         </div>
+
+         <textarea
+          id='letter'
+          name='letter'
+          rows={8}
+          className='block w-full rounded-md border-0 py-1.5 p-5 text-white shadow-sm ring-1 ring-gray-600 sm:text-sm sm:leading-6'
+          defaultValue={''}
+          placeholder='محل نوشتن نامه زودیاک'
+         />
+
+         <div className='bg-gray-800 px-4 py-3 sm:flex sm:flex-row sm:px-6'>
+          <button
+           type='button'
+           onClick={() => setOpen(false)}
+           className='inline-flex w-full justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-red-500 sm:mr-3 sm:w-auto'
+          >
+           فقط گرداننده نامه رو ببنده
+          </button>
+         </div>
+        </DialogPanel>
+       </div>
+      </div>
+     </Dialog>
 
      <textarea
       id='comment'
